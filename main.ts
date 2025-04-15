991054f3b8cfd9f728749a9e3e704b276ec3219f
ikhyeons <skantrkwl789@naver.com>
2025-04-15 21:52:16 +0900

    Init : Building First Electron App

---
+++ main.ts
@@ -0,0 +1,27 @@
+// controll electron's main process
+// controll app's life cycle
+
+import { app, BrowserWindow } from "electron";
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
