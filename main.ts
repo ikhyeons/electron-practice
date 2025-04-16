bde860d14743c518b46de22521350fd9264f278e
ikhyeons <skantrkwl789@naver.com>
2025-04-16 13:28:26 +0900

    Feat: Connect the renderer and main processes using a preloader

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
 // controll electron's main process
 // controll app's life cycle
 
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, ipcMain } from "electron";
+import path from "path";
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
 };
 
 app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
   createWindow();
-
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
